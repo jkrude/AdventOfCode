b8ef73b3b12b5b09a2dd4b8303297cd687f7ed03
jkrude <jakob.krude@hotmail.com>
2021-12-06 12:49:30 +0100

    Day6

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,43 @@
+import java.io.FileNotFoundException
+
+
+class Day6 {
+    companion object {
+
+        fun readReport(): List<Int> =
+            object {}.javaClass.getResource("day6.txt")?.readText()?.split(",")?.map { it.toInt() }
+                ?: throw FileNotFoundException()
+
+        fun partOne(data: List<Int>): Int {
+            // Naive implementation as task suggests.
+            val fishies = data.toMutableList()
+            repeat(80) {
+                for (fishIdx in fishies.indices) {
+                    fishies[fishIdx] = if (fishies[fishIdx] == 0) 6.also { fishies.add(8) } else fishies[fishIdx] - 1;
+                }
+            }
+            return fishies.size
+        }
+
+        fun partTwo(data: List<Int>): Long {
+            // Position in array == day until reproduction
+            // Each day the dayUntilReproduce decreases by one -> move all - 1 -> or move idx + 1.
+            // Use ring-buffer where idx are all fish with dayUntilReproduction == 0..
+            // reproduction means there are fishies[idx] more fish for dayUntilReproduce==7
+            val fishies: Array<Long> = Array(9) { 0L }
+            data.groupBy { it }.forEach { (key, value) -> fishies[key] = value.size.toLong() }
+            var idx = 0
+            repeat(256) {
+                fishies[(idx + 7) % 9] += fishies[idx]
+                idx = (idx + 1) % 9
+            }
+            return fishies.sum()
+        }
+    }
+}
+
+fun main() {
+    val data = Day6.readReport()
+    //println(d.partOne(data))
+    println(Day6.partTwo(data))
+}
