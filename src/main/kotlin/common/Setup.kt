62a76c4d43d6c3c569b7354a3ddfa2a17689d40b
jkrude <jakob.krude@hotmail.com>
2023-11-22 20:55:16 +0100

    Replace session cookie with .env file wink, Long by default, option for lines vs full text

--- src/main/kotlin/common/Setup.kt
+++ src/main/kotlin/common/Setup.kt
@@ -11,10 +11,10 @@ object Setup {
 
 
     val sessionCookie: String =
-        "session=YOUR COOKIE HERE"
+        Setup.javaClass.getResource("../session.env")?.readText() ?: error("Could not read session.env file")
 
     fun downloadFileContent(fromWhere: URI): String {
-        val client = HttpClient.newBuilder().build();
+        val client = HttpClient.newBuilder().build()
         val request = HttpRequest.newBuilder()
             .uri(fromWhere)
             .setHeader("Cookie", sessionCookie)
@@ -31,7 +31,7 @@ object Setup {
             .writeText(content)
     }
 
-    fun writeTestClass(year: Int, day: Int) {
+    fun writeTestClass(year: Int, day: Int, readLines: Boolean = true) {
         require(File("./src/test/kotlin/y$year/").exists())
         val testClassCode = """
             package y$year
@@ -41,17 +41,17 @@ object Setup {
 
             internal class Day${day}Test {
 
-                val getTestData = ""${'"'}
+                val testData = ""${'"'}
                 
-                ""${'"'}.trimIndent().lines()
+                ""${'"'}.trimIndent()${if (readLines) ".lines()" else ""}
 
                 @Test
                 internal fun partOne() {
-                    //assertEquals(, y$year.Day$day.partOne(getTestData))
+                    //assertEquals(L, y$year.Day$day.partOne(testData))
                 }
                 @Test
                 internal fun partTwo() {
-                    //assertEquals(,y$year.Day$day.partTwo(getTestData))
+                    //assertEquals(L,y$year.Day$day.partTwo(testData))
                 }
             }
         """.trimIndent()
@@ -59,20 +59,24 @@ object Setup {
             .writeText(testClassCode)
     }
 
-    fun writeClass(year: Int, day: Int) {
+    fun writeClass(year: Int, day: Int, readLines: Boolean = true) {
+        val argument = if (readLines) "lines: List<String>" else "input: String"
+        val readInput = if (readLines) "readFileLines($day,$year)" else "readFileText($day,$year)"
+        val import = if (readLines) "readFileLines" else "readFileText"
         val classCode = """
             package y$year
 
-            import common.readFileLines
+            import common.$import
 
             object Day$day {
-                fun partOne(lines: List<String>): Int = TODO()
-                fun partTwo(lines: List<String>): Int = TODO()
+                fun partOne($argument): Long = TODO()
+                fun partTwo($argument): Long = TODO()
             }
 
             fun main() {
-                println(Day${day}.partOne(readFileLines($day,$year)))
-                //println(Day${day}.partTwo(readFileLines($day,$year)))
+                val input = $readInput
+                println(Day${day}.partOne(input))
+                //println(Day${day}.partTwo(input))
             }
         """.trimIndent()
         val file = File("./src/main/kotlin/y$year/Day${day}.kt")
@@ -90,25 +94,31 @@ object Setup {
         }
     }
 
-    fun forDate(day: Int, month: Int = Calendar.DECEMBER, year: Int = Calendar.getInstance().get(Calendar.YEAR)) {
+    fun forDate(
+        day: Int,
+        month: Int = Calendar.DECEMBER,
+        year: Int = Calendar.getInstance().get(Calendar.YEAR),
+        readLines: Boolean = true
+    ) {
         require(year >= 2016 && month == Calendar.DECEMBER) { "There  exists no puzzle for today" }
         val inputData = downloadFileContent(
             URI.create("https://adventofcode.com/$year/day/$day/input")
         )
         createFoldersIfNotExists(year)
         writeInputData(year, day, inputData)
-        writeClass(year, day)
-        writeTestClass(year, day)
+        writeClass(year, day, readLines)
+        writeTestClass(year, day, readLines)
     }
 
-    fun forToday() = forDate(
+    fun forToday(readLines: Boolean = true) = forDate(
         Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
         Calendar.getInstance().get(Calendar.MONTH),
-        Calendar.getInstance().get(Calendar.YEAR)
+        Calendar.getInstance().get(Calendar.YEAR),
+        readLines
     )
 
 }
 
 fun main() {
-    Setup.forDate(7, year = 2020)
+    Setup.forDate(20, year = 2020, readLines = false)
 }
