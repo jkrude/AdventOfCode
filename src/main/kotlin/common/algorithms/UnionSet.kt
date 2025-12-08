5cc37a4036b99f7ec5598fc7a7edda6c22d219e3
jkrude <jakob.krude@hotmail.com>
2025-12-08 16:36:32 +0100

    Improve UnionSet to track connected components

--- src/main/kotlin/common/algorithms/UnionSet.kt
+++ src/main/kotlin/common/algorithms/UnionSet.kt
@@ -3,6 +3,12 @@ package common.algorithms
 class UnionSet<T>() {
 
     private val parents = mutableMapOf<T, T>()
+    var numSets: Int = 0
+        private set
+    private val sizes = mutableMapOf<T, Int>()
+
+    val elements: Set<T> get() = parents.keys
+    val size: Int get() = parents.size
 
     constructor(initialElements: Iterable<T>) : this() {
         initialElements.forEach { add(it) }
@@ -10,34 +16,48 @@ class UnionSet<T>() {
 
     fun add(element: T) {
         parents[element] = element
+        sizes[element] = 1
+        numSets++
     }
 
-    fun unionSet(a: T, b: T) {
+    fun contains(element: T): Boolean = element in parents
+
+    fun unionSet(a: T, b: T): Boolean {
         require(a in parents && b in parents)
-        val parentA = findSet(a)
-        val parentB = findSet(b)
-        if (parentA == parentB) return
-        parents[parentB] = parents[parentA]!! // Could be optimized
+        val rootA = findSet(a)
+        val rootB = findSet(b)
+        if (rootA == rootB) return false
+
+        // Union by size: attach smaller tree to larger tree
+        val sizeA = sizes[rootA]!!
+        val sizeB = sizes[rootB]!!
+
+        if (sizeA < sizeB) {
+            parents[rootA] = rootB
+            sizes[rootB] = sizeB + sizeA
+        } else {
+            parents[rootB] = rootA
+            sizes[rootA] = sizeA + sizeB
+        }
+
+        numSets--
+        return true
     }
 
-    fun unionSetAddIfAbsent(a: T, b: T) {
+    fun unionSetAddIfAbsent(a: T, b: T): Boolean {
         if (a !in parents) add(a)
         if (b !in parents) add(b)
         return unionSet(a, b)
     }
 
-    fun findSet(a: T): T {
-        var curr = a
-        val visited = mutableListOf(a)
-        while (parents[curr] != curr) {
-            curr = parents[curr]!!
-            visited.add(curr)
+    fun findSet(element: T): T {
+        val p = parents[element]!!
+        if (p == element) {
+            return element
         }
-        for (node in visited) { // Path compression
-            parents[node] = curr
-        }
-        return curr
+        // Path Compression: Point directly to the root
+        val root = findSet(p)
+        parents[element] = root
+        return root
     }
-
-
 }
